02723ad5cbbf10e4c6e5ba0959f0f8ffd5c6d071
Jeet Patel <jeet@steinnlabs.com>
2019-12-11 19:40:17 +0530

    created databases

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,16 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import authtoken
+# from django.contrib.auth.models import authtoken
 from django.contrib.auth.models import BaseUserManager
-from django.contrib.auth.models import PermissionMixin
+from django.contrib.auth.models import PermissionsMixin
 
 
-class UserProfile(AbstractBaseUser, PermissionMixin):
+class UserProfileManager(BaseUserManager):
+    """Manage for user profile"""
+
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise valueError('users must have have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+        def create_super_user(self, email, name, password):
+            """create and asave a new super user"""
+            user = self.create_user(email, name, password)
+
+            user.is_superuser = True
+            user.is_staff = True
+            user.save(using=self._db)
+
+            return user
+
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
-    name = model.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=Falss)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
