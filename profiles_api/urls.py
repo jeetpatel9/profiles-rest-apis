0a7b364be0c410f7f446d25e40d37ec1874dbbe2
Jeet Patel <jeet@steinnlabs.com>
2019-12-28 00:48:26 +0530

    view set completed

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
+router = DefaultRouter()
+router.register('hello-viewsets', views.HelloViewSet, base_name = 'hello-viewsets')
 
 urlpatterns = [
-    path('hello-view/', views.HelloAPIView.as_view())
+    path('hello-view/', views.HelloAPIView.as_view()),
+    path('', include(router.urls))
 ]
