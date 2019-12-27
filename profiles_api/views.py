35d672ee5fd3bd61426cf8a61bab98927e5f169d
Jeet Patel <jeet@steinnlabs.com>
2019-12-27 17:30:29 +0530

    created first view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # Create your views here.
+
+class HelloAPIView(APIView):
+    """docstring for HelloAPIView."""
+
+    def get(self, request, formate=None):
+
+        an_apiview = [
+            'ahskbajc',
+            'ads',
+            'ahskbajc',
+            'ads',
+            'ahskbajc',
+            'ads',
+        ]
+
+        return Response({
+        'message' : 'Hello',
+        'an_apiview' : an_apiview
+        })
